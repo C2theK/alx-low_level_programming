6b873655a00375882ecb4d48a9615afcd85f3d60
c2thek <charleskuto@gmail.com>
2023-02-16 18:31:17 +0000

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- *  * main - A program that prints a line with puts function
- *   * Return: 0 (Success)
- *    */
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art, \n");
-	return(0);}
+puts("with proper grammar, but the outcome is a piece of art, \n");
+return(0);}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - A program that prints the size of various computer types
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(a));
+print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(b));
+print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(c));
+print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(d));
+print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(f));
+return (0);
