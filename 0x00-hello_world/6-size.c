b39229fd624e9694095ff7ae7206f100a1bb0590
c2thek <charleskuto@gmail.com>
2023-02-18 08:44:15 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,6 @@
 * main - A program that prints the size of various computer types
 * Return: 0 (Success)
 */
-
 int main(void)
 {
 	char a;
@@ -11,11 +10,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(a));
-print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(b));
-print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(c));
-print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(d));
-print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(f));
-return (0);
 
+print f("size of char: %1u byte(s)\n", (unsigned long)sizeof(a));
+print f("size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
+print f("size of long int: %1u byte(s)\n", (unsigned long)sizeof(c));
+print f("size of long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
+print f("size of float: %1u byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
