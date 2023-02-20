199152f530fa9aee544644edb22a0ec6a8224ad6
c2thek <charleskuto@gmail.com>
2023-02-20 09:03:55 +0000

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- *Print all the letters except q and e
- *You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
- *All your code should be in the main function
- *    * Return: Always 0 (Success)
+ * main - Entry point
+ * Print all the letters except q and e
+ * You can only use the putchar function
+ * All your code should be in the main function
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-		puts("\"abcdfghijklmnoprstuvwxyz");
+		puts("abcdfghijklmnoprstuvwxyz");
 			return (0);
 }
