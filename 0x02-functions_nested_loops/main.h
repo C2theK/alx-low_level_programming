9f228a579bcfb8daf0a786e374feb9fd8e50f577
c2thek <charleskuto@gmail.com>
2023-02-21 11:36:41 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,9 @@
+#include "main.h"
+/**
+ * main - main.h
+ *
+ * Return: 0
+int main(void)
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -9,5 +15,8 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
+void print_to_98(int n)
 void print_times_table(int n);
+
+*/
+
