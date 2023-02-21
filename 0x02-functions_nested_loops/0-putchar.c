3cdf2853bbe0a219de79a466f3bfafdeaa04ec6c
c2thek <charleskuto@gmail.com>
2023-02-21 12:29:01 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,14 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * main - print _putchar
- * written ck
- * Return: 0
-*/
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On the success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
-int main(void)
-{
-	char word[] = _putchar;
-	int i;
-
-	for (i = 0; i <= 7; i++)
-	{
-		_putchar(word[i];
-	}
-	_putchar('\n')
+int_putchar(char c)
+}
+	return (write(1, &c, 1));
 }
