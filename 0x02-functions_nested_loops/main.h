e112c5fc91a12338839e20d9ce582368c90bc96a
c2thek <charleskuto@gmail.com>
2023-02-21 11:04:11 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,13 @@
-#include "main.h"
-/**
- * main - entry block
- * Description: prints_putchar followed by a new line
- * Return: 0
- */
-int main(void)
-{
-	char c[8] = "_putchar";
-
-	int i = 0:
-
-	while (i < 8)
-	{
-		_putchar([i]);
-		i++;
-	}
-	_putchar('\n')
-	return 0;
-
-}
-
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
