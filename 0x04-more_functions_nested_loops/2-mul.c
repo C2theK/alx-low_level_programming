9d5a1678247642433ca8244eab378c4519ac7c54
c2thek <charleskuto@gmail.com>
2023-02-23 10:29:04 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* mul - multiple integers or checks for the two integers
+* @a: a - Variable
+* @b: b - variable
+* code by ck
+* Return: Always 0.
+*/
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
