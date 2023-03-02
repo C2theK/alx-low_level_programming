a270995c6e080651e0d53dbeed6d5cd94a805714
C2theK <charleskuto@gmail.com>
2023-03-02 07:57:01 +0000

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,14 +13,14 @@
 int main(void);
 {
 	int n;
-	int a[5];
+	int b[5];
 	int *p;
 
 
-	a[2] = 1024;
+	b[2] = 1024;
 	p = &n;
 
 
-	printf("a[2] = %d\n", a[2]);
+	printf("b[2] = %d\n", b[2]);
 	return (0);
 }
