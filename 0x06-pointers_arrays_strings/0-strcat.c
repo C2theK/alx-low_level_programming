a436646770b14ca06e92255057637492ab59ad43
C2theK <charleskuto@gmail.com>
2023-03-02 09:23:51 +0000

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
-* strcat - Concatenates the string pointed to by @src, including the x
-* null byte, to the end of the string pointed to by @dest.
-* @dest: A pointer to the string to be concatenated upon.
-* @src: The source string to be appended to @dest.
+* strcat - Concatenates the string
+* @dest:input value
+* @src: input value
 * Return: A pointer to the destination string @dest.
 */
 
