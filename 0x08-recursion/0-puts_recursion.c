c1ebff2ddfd2d37b3620fa0fb4463c59b5f8c181
C2theK <charleskuto@gmail.com>
2023-03-08 16:28:44 +0000

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,14 @@
 */
 void _puts_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
 {
+_putchar('\n');
+return;
+}
 _putchar(*s);
-_puts_recursion(s + 1);
+s++;
+_puts_recursion(s);
 }
 
-else
-_putchar('\n');
-}
 
