edfac4f02ae1f6e21e8c1d5fead00543cb4ab98e
C2theK <charleskuto@gmail.com>
2023-03-13 10:34:05 +0000

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
 * Description: create array of size and assign char c
 * Return: pointer to array, NULL if fail
 */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 char *arr;
 int i = 0;
